c942ccb5e2cbc94d838c337a500524d632c3ed6c
DmACKGL <francosanllhei@gmail.com>
2019-08-06 13:50:13 -0400

    Getters Setters & Stuff

--- src/peliculas/Pelicula.java
+++ src/peliculas/Pelicula.java
@@ -6,18 +6,71 @@ class Pelicula {
     private String genero;
     private String actorActriz;
 
-    Pelicula(String titulo, String genero, int anno, String actorActriz){
+    Pelicula(String titulo, String genero, int anno, String actorActriz) {
         this.titulo = titulo;
         this.genero = genero;
         this.anno = anno;
-        this. actorActriz = actorActriz;
+        this.actorActriz = actorActriz;
     }
+
+    Pelicula(String titulo, int anno) {
+        this.titulo = titulo;
+        this.anno = anno;
+    }
+
+    /**
+     * Getters and Setters
+     * ALT + Insertar
+     */
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    void setAnno(int anno) {
+        if (anno >= 1885)
+            this.anno = anno;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getActorActriz() {
+        return actorActriz;
+    }
+
+    void setActorActriz(String actorActriz) {
+        this.actorActriz = actorActriz;
+    }
+
+    /**
+     * End Getters and Setters
+     */
 
     void mostrarInfoPeicula(){
         System.out.println("=====================================");
         System.out.println("Titulo: "+this.titulo);
-        System.out.println("Genero: "+this.genero);
-        System.out.println("Año: "+this.anno);
+        if (this.genero != null)
+            System.out.println("Genero: "+this.genero);
+        else
+            System.out.println("Genero: Desconocido");
+        if (this.genero != null)
+            System.out.println("Año: "+this.anno);
+        else
+            System.out.println("Año: Desconocido");
         System.out.println("Actor/Actriz: "+this.actorActriz);
     }
 }
--- src/peliculas/programa.java
+++ src/peliculas/programa.java
@@ -4,7 +4,13 @@ public class programa {
     public static void main(String[] args){
         Pelicula pelicula01 = new Pelicula("Titanic", "Romance", 1997, "Kate Winstel");
         Pelicula pelicula02 = new Pelicula("Toy Story", "Animacion", 1995, "Buzz");
+
         pelicula01.mostrarInfoPeicula();
         pelicula02.mostrarInfoPeicula();
+
+        Pelicula pelicula03 = new Pelicula("Iron Man", 2008);
+        pelicula03.setGenero("Accion");
+        pelicula03.setAnno(2008);
+        pelicula03.mostrarInfoPeicula();
     }
 }
